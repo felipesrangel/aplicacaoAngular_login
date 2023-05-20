dacb768d43211ddd14c2b96da35bc78bd3c8f8fe
Felipe Rangel <78965577+felipesrangel@users.noreply.github.com>
2023-05-20 15:38:08 -0300

    criando a validacao no email e senha via services

--- src/app/core/components/auth/auth.module.ts
+++ src/app/core/components/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
+
 import { SignComponent } from './pages/sign/sign.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -14,8 +14,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     CommonModule,
     AuthRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    ReactiveFormsModule
+
   ]
 })
 export class AuthModule { }
